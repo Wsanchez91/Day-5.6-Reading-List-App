76e967f3856877be010c64dafc2f49c05116e076
William Sanchez <williamsanchez.8055@gmail.com>
2025-04-15 10:22:47 -0400

    finished

--- script.js
+++ script.js
@@ -10,27 +10,39 @@ const addBtn = document.querySelector("#add-book-btn");
 // This shows the stored books in the localStorage, or if it's empty it will give an empty array.
 const storedBooks = JSON.parse(localStorage.getItem("books")) || [];
 
-// Listen for Enter key press in title input
-titleInput.addEventListener("keydown", (e) => {
-  if (e.key === "Enter") {
-    e.preventDefault();
-    addBook();
-  }
-});
+const renderBookList = (bookObj) => {
+  const bookDiv = document.createElement("div");
+  bookDiv.setAttribute("data-status", bookObj.status);
 
-// Listen for Enter key press in author input
-authorInput.addEventListener("keydown", (e) => {
-  if (e.key === "Enter") {
-    e.preventDefault();
-    addBook();
-  }
-});
+  const titleP = document.createElement("p");
+  titleP.innerHTML = `<strong>Title: </strong>${bookObj.title}`;
 
-// Listen for click on the Add Book button
-addBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  addBook();
-});
+  const authorP = document.createElement("p");
+  authorP.innerHTML = `<strong>Author: </strong>${bookObj.author}`;
+
+  const statusP = document.createElement("p");
+  statusP.innerHTML = `<strong>Status: </strong>${bookObj.status}`;
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "Delete";
+
+  deleteBtn.addEventListener("click", () => {
+    bookDiv.remove();
+    const index = storedBooks.findIndex((b) => b.id === bookObj.id);
+
+    if (index !== -1) {
+      storedBooks.splice(index, 1);
+      localStorage.setItem("books", JSON.stringify(storedBooks));
+    }
+  });
+
+  bookDiv.appendChild(titleP);
+  bookDiv.appendChild(authorP);
+  bookDiv.appendChild(statusP);
+  bookDiv.appendChild(deleteBtn);
+
+  bookList.appendChild(bookDiv);
+};
 
 /* Function: addBook- Adds a new book and updates storage */
 const addBook = () => {
@@ -45,6 +57,62 @@ const addBook = () => {
     status: statusOpt,
   };
 
-  bookList.push(bookObj);
-  localStorage.setItem("books", JSON.stringify(bookList));
+  storedBooks.push(bookObj);
+  localStorage.setItem("books", JSON.stringify(storedBooks));
+
+  renderBookList(bookObj);
 };
+
+const loadBooks = () => {
+  storedBooks.forEach((books) => {
+    renderBookList(books);
+  });
+};
+
+// Listen for Enter key press in title input
+titleInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    e.preventDefault();
+    addBook();
+    titleInput.value = "";
+    authorInput.value = "";
+  }
+});
+
+// Listen for Enter key press in author input
+authorInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    e.preventDefault();
+    addBook();
+    titleName.value = "";
+    authorName.value = "";
+  }
+});
+
+// Listen for click on the Add Book button
+addBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  addBook();
+  titleName.value = "";
+  authorName.value = "";
+});
+
+allBtn.addEventListener("click", () => {
+  document.querySelectorAll("#current-book-list > div").forEach((bookDiv) => {
+    bookDiv.style.display = "flex";
+  });
+});
+readBtn.addEventListener("click", () => {
+  document.querySelectorAll("#current-book-list > div").forEach((bookDiv) => {
+    const status = bookDiv.getAttribute("data-status"); 
+    bookDiv.style.display = status.toLocaleLowerCase() === "read" ? "flex": "none";
+  });
+});
+unreadBtn.addEventListener("click", () => {
+  document.querySelectorAll("#current-book-list > div").forEach((bookDiv) => {
+    const status = bookDiv.getAttribute("data-status");
+    bookDiv.style.display = status.toLocaleLowerCase() === "unread"? "flex": "none";
+  });
+});
+
+window.addEventListener("DOMContentLoaded", loadBooks);
